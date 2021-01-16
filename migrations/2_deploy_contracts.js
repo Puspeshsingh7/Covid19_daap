47f468812857b1acfeb0020abd31ffd242fbdc8e
Puspesh Kumar <puspeshkumar2017@gmail.com>
2021-01-16 22:54:26 +0530

    Delete migrations directory

--- migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-// JavaScript source code
-const Covid19_blockchain = artifacts.require("Covid19_blockchain");
-
-module.exports = function (deployer) {
-    deployer.deploy(Covid19_blockchain);
-};
